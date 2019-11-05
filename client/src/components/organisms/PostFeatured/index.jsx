6438475ee58ac489a1594d823172adf5b747ac16
rafaelgiro <rafael.hyf@gmail.com>
2019-11-05 18:41:46 -0300

    imagem do post destaque em collage

--- client/src/components/organisms/PostFeatured/index.jsx
+++ client/src/components/organisms/PostFeatured/index.jsx
@@ -4,7 +4,11 @@ const PostFeatured = () => {
   return (
     <div className="post-featured">
       <div>Texto</div>
-      <div>Imagem</div>
+      <div className="post-featured__image-container">
+        <div className="post-featured__image post-featured__image-1" />
+        <div className="post-featured__image post-featured__image-2" />
+        <div className="post-featured__image post-featured__image-3" />
+      </div>
     </div>
   );
 };

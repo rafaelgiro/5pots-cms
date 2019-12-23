6e9d422f9708b8a681019303f820fba55cb9b499
rafaelgiro <rafael.hyf@gmail.com>
2019-12-23 11:59:24 -0300

    posts em cards com grid de tamanho dinâmica

--- client/src/components/organisms/PostList/index.jsx
+++ client/src/components/organisms/PostList/index.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import api from "../../../services/api";
-import PostCard from "../../molecules/PostCard";
+import api from '../../../services/api';
+import PostCard from '../../molecules/PostCard';
 
 export default function PostList() {
   const [posts, setPosts] = useState();
   // eslint-disable-next-line no-unused-vars
-  const [postType, setPostType] = useState("card");
+  const [postType, setPostType] = useState('card');
   const [heightSize, setHeightSize] = useState([0, 0, 0]);
 
   // Carrega os posts
   useEffect(() => {
     async function loadPosts() {
-      const response = await api.get("/posts");
+      const response = await api.get('/posts');
 
       setPosts(response.data);
     }
@@ -24,26 +24,58 @@ export default function PostList() {
   const renderPosts = () => {
     // Verifica se os posts foram carregados
     if (posts) {
-      const postsList = posts.map((post, index) => {
-        return (
-          <PostCard
-            key={index}
-            post={post}
-            number={index}
-            heightSize={heightSize}
-            setHeightSize={setHeightSize}
-          />
-        );
+      const postsC1 = posts.map((post, index) => {
+        if (index % 3 === 0) {
+          return (
+            <PostCard
+              key={index}
+              post={post}
+              number={index}
+              heightSize={heightSize}
+              setHeightSize={setHeightSize}
+            />
+          );
+        }
       });
-      return postsList;
+
+      const postsC2 = posts.map((post, index) => {
+        if (index % 3 === 1) {
+          return (
+            <PostCard
+              key={index}
+              post={post}
+              number={index}
+              heightSize={heightSize}
+              setHeightSize={setHeightSize}
+            />
+          );
+        }
+      });
+
+      const postsC3 = posts.map((post, index) => {
+        if (index % 3 === 2) {
+          return (
+            <PostCard
+              key={index}
+              post={post}
+              number={index}
+              heightSize={heightSize}
+              setHeightSize={setHeightSize}
+            />
+          );
+        }
+      });
+      return (
+        <>
+          <div className="post-list--card__column">{postsC1}</div>
+          <div className="post-list--card__column">{postsC2}</div>
+          <div className="post-list--card__column">{postsC3}</div>
+        </>
+      );
     }
     // TODO: componente esqueleto
-    return "loading...";
+    return 'loading...';
   };
 
-  return (
-    <section className={`post-list post-list--${postType}`}>
-      {renderPosts()}
-    </section>
-  );
+  return <section className={`post-list post-list--${postType}`}>{renderPosts()}</section>;
 }
--- server/src/app/controllers/PostController.js
+++ server/src/app/controllers/PostController.js
@@ -3,103 +3,103 @@ class BankController {
   async index(req, res) {
     const posts = [
       {
-        title: "TEAMFIGHT TACTICS: A ASCENSÃO DOS ELEMENTOS",
+        title: 'TEAMFIGHT TACTICS: A ASCENSÃO DOS ELEMENTOS',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/10/LOL_CMS_314_Tile_01-Feature_H40-V50-min_94ucpce02v5cgf3ootvq.jpg",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/10/LOL_CMS_314_Tile_01-Feature_H40-V50-min_94ucpce02v5cgf3ootvq.jpg',
         commentsCount: 24,
-        category: "artigo"
+        category: 'artigo'
       },
       {
-        title: "ATUALIZAÇÃO DO PBE 25/10 NOVAS ARENAS DO TFT",
+        title: 'ATUALIZAÇÃO DO PBE 25/10 NOVAS ARENAS DO TFT',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/12/HQ_Header_Ask_Riot_Preseaosn_f8i1ljevnti7wpo7oh2n.png",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/12/HQ_Header_Ask_Riot_Preseaosn_f8i1ljevnti7wpo7oh2n.png',
         commentsCount: 12,
-        category: "pbe"
+        category: 'pbe'
       },
       {
-        title: "Pergunte a riot: Skin decente pro Ivern",
+        title: 'Pergunte a riot: Skin decente pro Ivern',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/08/Banner_Image_Ask_Riot_zp7rnwkbgfttb0v5frin.jpg",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/08/Banner_Image_Ask_Riot_zp7rnwkbgfttb0v5frin.jpg',
         commentsCount: 345,
-        category: "riot"
+        category: 'riot'
       },
       {
-        title: "TEAMFIGHT TACTICS: A ASCENSÃO DOS ELEMENTOS",
+        title: 'TEAMFIGHT TACTICS: A ASCENSÃO DOS ELEMENTOS',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/10/LOL_CMS_314_Tile_01-Feature_H40-V50-min_94ucpce02v5cgf3ootvq.jpg",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/11/Banner_TD_Ask_Riot_0nwizm4e7mrinfgu73px.jpg',
         commentsCount: 24,
-        category: "artigo"
+        category: 'artigo'
       },
       {
-        title: "ATUALIZAÇÃO DO PBE 25/10 NOVAS ARENAS DO TFT",
+        title: 'ATUALIZAÇÃO DO PBE 25/10 NOVAS ARENAS DO TFT',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/12/HQ_Header_Ask_Riot_Preseaosn_f8i1ljevnti7wpo7oh2n.png",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/11/00_Banner_T2_Client_Image_9py3o8n8i6w2znjbu3zb.jpg',
         commentsCount: 12,
-        category: "pbe"
+        category: 'artigo'
       },
       {
-        title: "Pergunte a riot: Skin decente pro Ivern",
+        title: 'Pergunte a riot: Skin decente pro Ivern',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/08/Banner_Image_Ask_Riot_zp7rnwkbgfttb0v5frin.jpg",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/10/Banner_Preseason-1_dwfwpnp0byzkpe2hk65v.jpg',
         commentsCount: 345,
-        category: "riot"
+        category: 'pbe'
       },
       {
-        title: "TEAMFIGHT TACTICS: A ASCENSÃO DOS ELEMENTOS",
+        title: 'TEAMFIGHT TACTICS: A ASCENSÃO DOS ELEMENTOS',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/10/LOL_CMS_314_Tile_01-Feature_H40-V50-min_94ucpce02v5cgf3ootvq.jpg",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/09/01_Banner_Narrative-1_7v6f03bt5eum5f9m2uvd.jpg',
         commentsCount: 24,
-        category: "artigo"
+        category: 'artigo'
       },
       {
-        title: "ATUALIZAÇÃO DO PBE 25/10 NOVAS ARENAS DO TFT",
+        title: 'ATUALIZAÇÃO DO PBE 25/10 NOVAS ARENAS DO TFT',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/12/HQ_Header_Ask_Riot_Preseaosn_f8i1ljevnti7wpo7oh2n.png",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/09/02_Kayle-1_dbzd5p6b3xmgryf3tl0a.jpg',
         commentsCount: 12,
-        category: "pbe"
+        category: 'riot'
       },
       {
-        title: "Pergunte a riot: Skin decente pro Ivern",
+        title: 'Pergunte a riot: Skin decente pro Ivern',
         description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
       Origens e as Classes que você conheceu durante o beta.`,
         image:
-          "https://nexus.leagueoflegends.com/wp-content/uploads/2019/08/Banner_Image_Ask_Riot_zp7rnwkbgfttb0v5frin.jpg",
+          'https://nexus.leagueoflegends.com/wp-content/uploads/2019/09/04_Pantheon-1_n77cg0g5px5wekv3mgwv.jpg',
         commentsCount: 345,
-        category: "riot"
+        category: 'default'
       }
     ];
 
@@ -110,8 +110,7 @@ class BankController {
     const { id } = req.params;
 
     const post = {
-      title:
-        "Atualização do PBE: Uma atualização com um milhão de tentativas de balanceamento",
+      title: 'Atualização do PBE: Uma atualização com um milhão de tentativas de balanceamento',
       description: `A fase beta de Teamfight Tactics chegou ao fim, e os 
       elementos ascenderão na segunda temporada! Sendo nosso primeiro novo 
       conjunto, A Ascensão dos Elementos muda completamente os Campeões, as 
@@ -120,9 +119,9 @@ class BankController {
        rutrum purus aliquam magna adipiscing a pellentesque. 
       Condimentum quis neque lorem eu duis. Lectus pellentesque pharetra orci
        suspendisse curabitur.`,
-      image: "https://i.imgur.com/YLdeUgB.png",
+      image: 'https://i.imgur.com/YLdeUgB.png',
       commentsCount: 24,
-      color: "default"
+      color: 'default'
     };
 
     return res.json({ response: `Retornaww o post de id ${id}` });

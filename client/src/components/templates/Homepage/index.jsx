ebaae787514d8c97ebc267263b60bc8fc98ba512
Rafael Giro <rafael.hyf@gmail.com>
2019-12-24 22:59:26 -0300

    release de natal sem post list

--- client/src/components/templates/Homepage/index.jsx
+++ client/src/components/templates/Homepage/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import PostFeatured from "../../organisms/PostFeatured";
 import StoriesCards from "../../organisms/StoriesCards";
-import PostList from "../../organisms/PostList";
+//import PostList from "../../organisms/PostList";
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <PostFeatured />
       <StoriesCards />
-      <PostList />
+      {/* <PostList /> */}
     </div>
   );
 };

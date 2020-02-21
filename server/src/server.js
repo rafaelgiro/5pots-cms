1d74cf93f6eebcac97890de27fc85ad1f8d5f155
Willian Silva <willwln33@gmail.com>
2020-02-21 19:35:51 -0300

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 import app from "./app";
 
-app.listen(process.env.NODE_PORT || 5800);
+app.listen(process.env.NODE_PORT || 5000);
 
-console.log(`Server started at port ${process.env.NODE_PORT || 5800}`);
+console.log(`Server started at port ${process.env.NODE_PORT || 5000}`);
 
 export default app;

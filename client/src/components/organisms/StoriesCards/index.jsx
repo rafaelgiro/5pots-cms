9ba5679bc74efcc75c76280575f7764453a3cf2a
Rafael Giro <ra20145287@wipro.com>
2020-05-23 10:34:51 -0300

    Refatora Cards das principais notícias

--- client/src/components/organisms/StoriesCards/index.jsx
+++ client/src/components/organisms/StoriesCards/index.jsx
@@ -1,26 +1,45 @@
 import React from "react";
 import ScrollContainer from "react-indiana-drag-scroll";
 
-import StoriesController from "../../molecules/StoriesController";
-import StoriesSelfCard from "../../molecules/StoriesSelfCard";
-import StoriesLinkCard from "../../molecules/StoriesLinkCard";
+import Controller from "./Controller";
+import SelfCard from "./SelfCard";
+import LinkCard from "./LinkCard";
+import MainCard from "./MainCard";
 
-const post = {
-  image:
-    "https://playruneterra.com/assets/48b7bffe8468c1a99fabdd06f1897e8a709e3343/static/image/champions/Yasuo.jpg",
+const postMain = {
+  image: "https://imgur.com/cl4lZ4R.jpg",
+  category: "Notícias oficiais",
+  title: "Criando os Reinos Mech",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit rerum alias deserunt quidem sed mollitia reiciendis nobis officia, tempora eaque laboriosam eveniet corporis voluptates omnis porro earum incidunt dignissimos!",
+};
+
+const postSelf = {
+  image: "https://images5.alphacoders.com/105/thumb-1920-1057451.jpg",
   category: "Atualização do PBE",
   title: "Atualização do PBE 27/01: Sion te amo",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit rerum alias deserunt quidem sed mollitia reiciendis nobis officia, tempora eaque laboriosam eveniet corporis voluptates omnis porro earum incidunt dignissimos!",
+};
+
+const postLink = {
+  image:
+    "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltcf72c3444f9b8c68/5eb5bad33458fa2d446903dc/PS20_Volibear_Base.jpg",
+  category: "Novo Campeão",
+  title: "Volibear - A Tempestade Implacável",
+  description:
+    "Quando o trovão rugir, conhecerão o nome dele. Acompanhe o retorno de Volibear no resumo da live realizada em 9 de maio de 2020.",
 };
 
 const Stories = () => {
   return (
     <section className="stories">
-      <StoriesController />
+      <Controller />
 
       <ScrollContainer className="stories__cards">
-        <StoriesSelfCard post={post} />
-        <StoriesLinkCard />
-        <StoriesLinkCard />
+        <MainCard post={postMain} />
+        <SelfCard post={postSelf} />
+        <LinkCard post={postLink} />
       </ScrollContainer>
     </section>
   );
